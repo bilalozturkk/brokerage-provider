44ceb780e5014ab9508c5b490316101880507aef
Bilal Öztürk <ozturk@Bilals-MacBook-Pro.local>
2023-07-03 01:53:35 +0300

    update secured endpoint list

--- src/main/java/ozturk/assignment/assignment/security/WebSecurityConfig.java
+++ src/main/java/ozturk/assignment/assignment/security/WebSecurityConfig.java
@@ -63,8 +63,6 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/api/users/**").permitAll()
-                                .requestMatchers("/api/kafka/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
